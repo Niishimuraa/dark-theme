3f10a628fb3bb12af2d519943ab0942fbf8348d2
Niishimuraa <henriqueyudi123@gmail.com>
2024-11-21 10:13:20 -0300

    Site completo com rotas e chage theme aplicados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './core/pages/components/home/home.component';
+import { SobreComponent } from './core/pages/components/sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'sobre', component: SobreComponent }
+];
---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [ RouterModule ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  public icon: string = 'moon';
+
+  toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+
+    this.icon = this.icon === 'moon' ? 'sun' : 'moon';
+  }
+}
